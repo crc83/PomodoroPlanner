32fc7bc473db06c6a3b4cc9ad426e3b46bf74ce0
Sergiy Beley <Sergiy.Beley@gmail.com>
2017-09-15 21:05:58 +0300

    add mood kinds

---
+++ app/src/main/kotlin/org/crc83/pomodoroplanner/api/MoodKind.kt
@@ -0,0 +1,8 @@
+package org.crc83.pomodoroplanner.api
+
+/**
+ * Created by Sergiy Beley on 9/15/2017.
+ */
+class MoodKind {
+    val moods = listOf("deep.thinking", "paperwork", "read.investigate")
+}
